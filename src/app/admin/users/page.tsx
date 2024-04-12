34ab113568dc00985b3f70bfb99b8a7e939b2215
Rayan Selmi <72416925+rs-4@users.noreply.github.com>
2024-04-12 16:23:40 +0200

    feat: add dashboard

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -4,6 +4,7 @@ import { FiEdit2, FiTrash2, FiSearch } from "react-icons/fi";
 import Modal from "../../components/modalUser";
 import UserService from "../../services/userService";
 import { User } from "../../models/user";
+import Loader from "@/app/components/loader";
 
 const UsersPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -11,6 +12,7 @@ const UsersPage = () => {
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [usersData, setUserData] = useState<User[]>([]);
   const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchDeleteUser = async (id: number) => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}user/${id}`, {
@@ -33,6 +35,7 @@ const UsersPage = () => {
       });
       const usersData = await res.json();
       setUserData(usersData.data);
+      setIsLoading(false);
     };
     getUser();
   }, []);
@@ -63,7 +66,6 @@ const UsersPage = () => {
   return (
     <div className="p-5">
       <div className="mb-5 flex items-center gap-2">
-        <FiSearch className="text-gray-400" />
         <input
           type="text"
           placeholder="Rechercher..."
@@ -71,59 +73,63 @@ const UsersPage = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
-      <div className="overflow-x-auto rounded-lg border border--2">
-        <table className="w-full text-left rounded-lg overflow-hidden">
-          <thead className="bg-black text-white">
-            <tr>
-              <th className="p-3">Nom</th>
-              <th className="p-3">Prénom</th>
-              <th className="p-3">Adresse Mail</th>
-              <th className="p-3">Adresse</th>
-              <th className="p-3">Code Postal</th>
-              <th className="p-3">Ville</th>
-              <th className="p-3">Actions</th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200">
-            {filteredUsers &&
-              filteredUsers.map((user) => (
-                <tr key={user.id} className="hover:bg-gray-100 group">
-                  <td className="p-3 whitespace-nowrap">{user.name}</td>
-                  <td className="p-3 whitespace-nowrap">{user.firstName}</td>
-                  <td className="p-3 whitespace-nowrap">{user.mail}</td>
-                  <td className="p-3 whitespace-nowrap">{user.adress}</td>
-                  <td className="p-3 whitespace-nowrap">{user.zipCode}</td>
-                  <td className="p-3 whitespace-nowrap">{user.city}</td>
-                  <td className="p-3">
-                    <div className="flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-                      <button
-                        className="p-2 text-blue-500 hover:text-blue-700 hover:bg-gray-300 rounded-full relative"
-                        onClick={() => handleEditUser(user)}
-                      >
-                        <span className="flex items-center justify-center">
-                          <FiEdit2 />
-                        </span>
-                      </button>
-                      <button
-                        className="p-2 text-red-500 hover:text-red-700 hover:bg-gray-300 rounded-full relative"
-                        onClick={() => fetchDeleteUser(user.id)}
-                      >
-                        <span className="flex items-center justify-center">
-                          <FiTrash2 />
-                        </span>
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-        <Modal
-          isOpen={showModal}
-          onClose={() => setShowModal(false)}
-          userData={selectedUser!}
-        />
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="overflow-x-auto rounded-lg border border--2">
+          <table className="w-full text-left rounded-lg overflow-hidden">
+            <thead className="bg-black text-white">
+              <tr>
+                <th className="p-3">Nom</th>
+                <th className="p-3">Prénom</th>
+                <th className="p-3">Adresse Mail</th>
+                <th className="p-3">Adresse</th>
+                <th className="p-3">Code Postal</th>
+                <th className="p-3">Ville</th>
+                <th className="p-3">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200">
+              {filteredUsers &&
+                filteredUsers.map((user) => (
+                  <tr key={user.id} className="hover:bg-gray-100 group">
+                    <td className="p-3 whitespace-nowrap">{user.name}</td>
+                    <td className="p-3 whitespace-nowrap">{user.firstName}</td>
+                    <td className="p-3 whitespace-nowrap">{user.mail}</td>
+                    <td className="p-3 whitespace-nowrap">{user.adress}</td>
+                    <td className="p-3 whitespace-nowrap">{user.zipCode}</td>
+                    <td className="p-3 whitespace-nowrap">{user.city}</td>
+                    <td className="p-3">
+                      <div className="flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+                        <button
+                          className="p-2 text-blue-500 hover:text-blue-700 hover:bg-gray-300 rounded-full relative"
+                          onClick={() => handleEditUser(user)}
+                        >
+                          <span className="flex items-center justify-center">
+                            <FiEdit2 />
+                          </span>
+                        </button>
+                        <button
+                          className="p-2 text-red-500 hover:text-red-700 hover:bg-gray-300 rounded-full relative"
+                          onClick={() => fetchDeleteUser(user.id)}
+                        >
+                          <span className="flex items-center justify-center">
+                            <FiTrash2 />
+                          </span>
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <Modal
+            isOpen={showModal}
+            onClose={() => setShowModal(false)}
+            userData={selectedUser!}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/app/components/input/index.tsx
+++ src/app/components/input/index.tsx
@@ -25,10 +25,7 @@ const Input = ({ title, name, link, onChange }: Props) => {
         </label>
         {link && (
           <div className="text-sm">
-            <Link
-              href={link.href}
-              className="font-semibold text-indigo-600 hover:text-indigo-500"
-            >
+            <Link href={link.href} className="font-semibold text-indigo-600">
               {link.title}
             </Link>
           </div>
@@ -40,7 +37,7 @@ const Input = ({ title, name, link, onChange }: Props) => {
           name={name}
           type={name}
           onChange={(e) => onChange(e.target.value)}
-          className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full  border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
         />
       </div>
     </div>
